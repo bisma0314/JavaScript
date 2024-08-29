770d57bc242d8c1614666b62b38eaacabfe9496a
bisma0314 <127329307+bisma0314@users.noreply.github.com>
2024-08-29 08:50:59 +0500

    Add files via upload

---
+++ Varibles.js
@@ -0,0 +1,8 @@
+const accountId = 12344;
+let aaccountName = "bisma";
+aaccountName = "rehman";
+var accountEmail = "bismarehmangc25@gmail.com";
+var accountEmail = "aliawan@gamil.com";
+var accountEmail = "hassanali@gmail.com";
+let  accountPassword = "730625";
+console.table([accountId,aaccountName,accountEmail,accountPassword]) ;
