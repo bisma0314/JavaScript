e3e63b94bd7c5671efc01ea7d43970920fdf0b53
bisma0314 <127329307+bisma0314@users.noreply.github.com>
2024-09-01 22:19:12 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,24 @@
+let  num = "134";
+num= true;
+num=0;
+num=null;
+console.log(typeof (num));
+let valueInNumber = Number(num);
+console.log(valueInNumber);
+//***************boolen*********************************** */
+let  value = "134";
+value= true;
+value=0;
+value=null;
+console.log(typeof (value));
+let valueInBoolen = Boolean(value);
+console.log(valueInBoolen);
+//**********************STRING******************************
+let  score = "134"
+score= true;
+score=0;
+score=null;
+console.log(typeof (score));
+let valueInString = String(score);
+console.log(valueInString);
+ 8 changes: 8 additions & 0 deletions8  
